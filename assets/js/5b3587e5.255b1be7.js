5f50cc2a06898b3a1cb04c8291950750d1140091
James <jimmidty.jammmers@gmail.com>
2023-06-14 09:47:55 -0500

    Built and published by Moonwave

---
+++ assets/js/5b3587e5.255b1be7.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[271],{83769:s=>{s.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
---
+++ assets/js/97f3293c.c0196324.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[197],{63891:e=>{e.exports=JSON.parse('{"sourceUrl":"https://github.com/canary-development/CanaryEngine/blob/master","baseUrl":"/docs/CanaryEngine/","classOrder":[],"apiCategories":[]}')}}]);
