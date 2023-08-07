f9109a2fa44368229a21d29a11f5c07ed737237c
James <jamesalexanderlittlefield@gmail.com>
2023-08-06 21:12:53 -0500

    Built and published by Moonwave

---
+++ assets/js/e949016b.d687f096.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[9668],{63629:e=>{e.exports=JSON.parse('[{"value":"Types","id":"types","level":2},{"value":"Properties","id":"properties","level":2},{"value":"Functions","id":"functions","level":2},{"value":".Animate","id":"Animate","level":3},{"value":".StopAnimation","id":"StopAnimation","level":3}]')}}]);
